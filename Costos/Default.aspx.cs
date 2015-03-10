c928556a7c01ecad148ca64f48539bdc47ffef20
Gustavo Cevallos <gstavocevallos@gmail.com>
2015-03-10 15:08:36 -0500

    dev

--- Costos/Default.aspx.cs
+++ Costos/Default.aspx.cs
@@ -9,13 +9,8 @@ namespace Costos
 {
     public partial class _Default : System.Web.UI.Page
     {
-        
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-
-
             if (!Page.IsPostBack)
             {
                 if (Convert.ToInt16(Session["con"]) > 0)
